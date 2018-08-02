98e4d792b2d8cfe18673d60de65549e3f2c33daf
leeseungheon <zakartayo@naver.com>
2018-08-02 17:52:51 +0900

    1. Firebase Authentication(Email) 연동 완료 2. 비밀번호 찾기 기능 완료 3. 회원가입( Email, Password, Password Check)

--- app/src/main/java/com/example/multimedia/juniorseoul/SignUpActivity.java
+++ app/src/main/java/com/example/multimedia/juniorseoul/SignUpActivity.java
@@ -1,6 +1,7 @@
 package com.example.multimedia.juniorseoul;
 
 import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,26 +11,36 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SignUpActivity extends AppCompatActivity {
     private static final String TAG = "SignupActivity";
 
-    @BindView(R.id.input_name)
-    EditText _nameText;
     @BindView(R.id.input_email)
     EditText _emailText;
     @BindView(R.id.input_password)
     EditText _passwordText;
+    @BindView(R.id.input_password_check)
+    EditText _passwordCheck;
     @BindView(R.id.btn_signup)
     Button _signupButton;
     @BindView(R.id.link_login)
     TextView _loginLink;
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+
         ButterKnife.bind(this);
 
         _signupButton.setOnClickListener(new View.OnClickListener() {
@@ -56,30 +67,37 @@ public class SignUpActivity extends AppCompatActivity {
             return;
         }
 
-        _signupButton.setEnabled(false);
 
         final ProgressDialog progressDialog = new ProgressDialog(SignUpActivity.this,
                 R.style.AppTheme_Dark_Dialog);
         progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Creating Account...");
+        progressDialog.setMessage("회원가입 진행중...");
         progressDialog.show();
 
-        String name = _nameText.getText().toString();
         String email = _emailText.getText().toString();
         String password = _passwordText.getText().toString();
 
         // TODO: Implement your own signup logic here.
-
-        new android.os.Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        // On complete call either onSignupSuccess or onSignupFailed
-                        // depending on success
-                        onSignupSuccess();
-                        // onSignupFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    new android.os.Handler().postDelayed(
+                            new Runnable() {
+                                public void run() {
+                                    // On complete call either onSignupSuccess or onSignupFailed
+                                    // depending on success
+                                    onSignupSuccess();
+                                    // onSignupFailed();
+                                    progressDialog.dismiss();
+                                }
+                            }, 3000);
+                } else {
+                    Toast.makeText(SignUpActivity.this, "이미 등록된 이메일이거나 암호가 4자리 미만, 13자리 초과입니다", Toast.LENGTH_LONG).show();
+                    progressDialog.dismiss();
+                }
+            }
+        });
     }
 
 
@@ -90,7 +108,6 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     public void onSignupFailed() {
-        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
 
         _signupButton.setEnabled(true);
     }
@@ -98,29 +115,29 @@ public class SignUpActivity extends AppCompatActivity {
     public boolean validate() {
         boolean valid = true;
 
-        String name = _nameText.getText().toString();
         String email = _emailText.getText().toString();
         String password = _passwordText.getText().toString();
-
-        if (name.isEmpty() || name.length() < 3) {
-            _nameText.setError("at least 3 characters");
-            valid = false;
-        } else {
-            _nameText.setError(null);
-        }
+        String password_check = _passwordCheck.getText().toString();
 
         if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            _emailText.setError("enter a valid email address");
+            _emailText.setError("이메일 형식이 올바르지 않습니다");
             valid = false;
         } else {
             _emailText.setError(null);
         }
 
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            _passwordText.setError("between 4 and 10 alphanumeric characters");
+        if (password.isEmpty() || password.length() < 4 || password.length() > 13) {
+            _passwordText.setError("4~13자리의 비밀번호를 입력하세요");
             valid = false;
         } else {
             _passwordText.setError(null);
+        }
+
+        if(!password.equals(password_check)){
+            _passwordCheck.setError("비밀번호가 일치하지 않습니다");
+            valid = false;
+        } else{
+            _passwordCheck.setError(null);
         }
 
         return valid;
